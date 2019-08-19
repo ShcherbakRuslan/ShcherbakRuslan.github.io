44390ed0ba55f5e24d5159b185407939f8f43469
Unknown <shcherbak.r.v@gmail.com>
2019-08-19 18:46:37 +1000

    Arka add

---
+++ s/Arka/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	var scene1 = document.getElementById('pil1');
+	var scene2 = document.getElementById('pil2');
+	var scene3 = document.getElementById('pil3');
+	var scene4 = document.getElementById('pil4');
+	var scene5 = document.getElementById('pil5');
+	var parallaxInstance = new Parallax(scene1);
+	var parallaxInstance = new Parallax(scene2);
+	var parallaxInstance = new Parallax(scene3);
+	var parallaxInstance = new Parallax(scene4);
+	var parallaxInstance = new Parallax(scene5);
+
+	// console.log(scene1);
+});
