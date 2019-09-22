c43f7d0bb337907621938c9ef28d50749f7531c9
Unknown <shcherbak.r.v@gmail.com>
2019-09-22 22:56:48 +1000

    BHX-Energy
    page main

--- s/bhx-energy/js/main.js
+++ s/bhx-energy/js/main.js
@@ -1,1 +1,38 @@
-$(".player").mb_YTPlayer();
+$('.player').mb_YTPlayer();
+
+$('.link').click(function(){
+	// console.log($(this).data('link'));
+
+	$('body').addClass('off');
+
+	if($(this).data('link')) {
+		var l = $(this).data('link');
+		$('#'+l).addClass('on');
+	} else {
+		console.log(0);
+	}
+});
+
+$('.close').click(function(){
+	$('.modal').removeClass('on');
+	$('body').removeClass('off');
+});
+
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
+
+$(window).scroll(function() {
+  if($(this).scrollTop() > $(window).height()) {
+    $('.up_btn').addClass('on');
+  } else {
+    $('.up_btn').removeClass('on');
+  }
+});
