99f1475d968c18805c2c501c5003c4620aa2b666
Unknown <shcherbak.r.v@gmail.com>
2019-11-14 00:05:44 +1000

    nsbg add

---
+++ s/nsbg/js/app.js
@@ -0,0 +1,5 @@
+$('#scratch').wScratchPad({
+  size: 200,          // The size of the brush/scratch.
+  bg: '../img/img2.jpg',  // Background (image path or hex color).
+  fg: '../img/img1.jpg',
+});
