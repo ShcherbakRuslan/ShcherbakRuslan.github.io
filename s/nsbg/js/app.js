d0fd6150c4a585523f73cd4e13b7306109232672
Unknown <shcherbak.r.v@gmail.com>
2019-11-14 00:12:37 +1000

    nsbg fix url img

--- s/nsbg/js/app.js
+++ s/nsbg/js/app.js
@@ -1,5 +1,5 @@
 $('#scratch').wScratchPad({
   size: 200,          // The size of the brush/scratch.
-  bg: '/img/img2.jpg',  // Background (image path or hex color).
-  fg: '/img/img1.jpg',
+  bg: 'img/img2.jpg',  // Background (image path or hex color).
+  fg: 'img/img1.jpg',
 });
